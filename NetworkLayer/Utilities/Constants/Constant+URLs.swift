d4537b68d56aa7f249a92aa63ee55f735686acc5
Sefa <aacarsefa@gmail.com>
2025-05-22 07:04:44 +0300

    Add: URLs

---
+++ NetworkLayer/Utilities/Constants/Constant+URLs.swift
@@ -0,0 +1,17 @@
+//
+//  Constant+URLs.swift
+//  NetworkLayer
+//
+//  Created by Sefa Acar on 22.05.2025.
+//
+
+import Foundation
+
+extension Constant {
+    
+    struct URLs {
+        
+        static let rmBaseURL = "https://rickandmortyapi.com/api/"
+        static let jphBaseURL = "https://jsonplaceholder.typicode.com/"
+    }
+}
---
+++ NetworkLayer/Utilities/Constants/Constant.swift
@@ -0,0 +1,10 @@
+//
+//  Constant.swift
+//  NetworkLayer
+//
+//  Created by Sefa Acar on 22.05.2025.
+//
+
+import Foundation
+
+struct Constant {}
